4f1ca17650700870d09d17f179b521344dc12687
Fernando Brûlé <fernandobrule@gmail.com>
2020-12-27 23:01:32 -0300

    intro scalaz validation

--- fragnostic-validator-api/src/main/scala/com/fragnostic/validator/api/ValidatorApi.scala
+++ fragnostic-validator-api/src/main/scala/com/fragnostic/validator/api/ValidatorApi.scala
@@ -1,24 +1,14 @@
 package com.fragnostic.validator.api
 
-import java.util.Locale
+import scalaz.{ NonEmptyList, ValidationNel }
 
-import scala.collection.mutable.ListBuffer
+import java.util.Locale
 
 trait ValidatorApi[T] {
 
   def doValidation(
     locale: Locale,
     item: T,
-    args: Map[String, String] = Map[String, String]()): Either[List[String], T]
-
-  def addError(error: String): List[String]
-
-  def addAllErrors(errors: ListBuffer[String]): List[String]
-
-  def addAllErrors(errors: List[String]): List[String]
-
-  def getErrors: List[String]
-
-  def reset(): Unit
+    args: Map[String, String] = Map[String, String]()): Either[NonEmptyList[String], T]
 
 }
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,12 @@ import sbt._
 import Keys._
 
 object Dependencies {
-  
-  // ext
-  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3" % "runtime"
-  lazy val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.25"
-  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.8" % "test"
-  
-  // fragnostic
-  lazy val fragnosticI18n = "com.fragnostic" % "fragnostic-i18n_2.13" % "0.1.15-SNAPSHOT"
-  
+
+  lazy val logbackClassic             = "ch.qos.logback" %  "logback-classic"                     % "1.2.3"
+  lazy val scalactic                  = "org.scalactic"  %% "scalactic"                           % "3.2.2" % "test"
+  lazy val scalatest                  = "org.scalatest"  %% "scalatest"                           % "3.2.2" % "test"
+  lazy val scalazCore                 = "org.scalaz"     %% "scalaz-core"                         % "7.3.2"
+
+  lazy val fragnosticI18n             = "com.fragnostic" % "fragnostic-i18n_2.13" % "0.1.15-SNAPSHOT"
+
 }
