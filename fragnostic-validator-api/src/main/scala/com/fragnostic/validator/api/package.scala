65df9c7b215f599885fc8a615904a226a49cbc38
Fernando Brûlé <fernandobrule@gmail.com>
2021-02-07 20:15:52 -0300

    add nelToList

--- fragnostic-validator-api/src/main/scala/com/fragnostic/validator/api/package.scala
+++ fragnostic-validator-api/src/main/scala/com/fragnostic/validator/api/package.scala
@@ -1,9 +1,12 @@
 package com.fragnostic.validator
 
-import scalaz.ValidationNel
+import scalaz.{ NonEmptyList, ValidationNel }
 
 package object api {
 
   type Validated[Z] = ValidationNel[String, Z]
+
+  def nelToList[T](nel: NonEmptyList[T]): List[T] =
+    nel.head :: nel.tail.toList
 
 }
