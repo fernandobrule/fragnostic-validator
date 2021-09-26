9cc35cb34cd4c48229e2868f844e2398773c4b5e
Fernando Brûlé <fernandobrule@gmail.com>
2021-09-26 13:15:26 -0300

    update validator signature

--- fragnostic-validator-api/src/main/scala/com/fragnostic/validator/api/ValidatorApi.scala
+++ fragnostic-validator-api/src/main/scala/com/fragnostic/validator/api/ValidatorApi.scala
@@ -1,14 +1,11 @@
 package com.fragnostic.validator.api
 
-import com.fragnostic.i18n.api.ResourceI18n
-
 import java.util.Locale
 
 trait ValidatorApi[T] {
 
   def validate(
     locale: Locale,
-    i18n: ResourceI18n,
     domain: String,
     t: T,
     params: Map[String, String] = Map.empty,
